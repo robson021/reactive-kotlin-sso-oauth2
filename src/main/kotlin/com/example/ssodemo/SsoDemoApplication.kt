36c2783974e36f65a5c5db4abe0a733873e57fb1
Robert Nowak <robertn@amazon.com>
2023-12-20 13:47:59 +0100

    initial commit

---
+++ src/main/kotlin/com/example/ssodemo/SsoDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ssodemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SsoDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SsoDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/ssodemo/SsoDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ssodemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SsoDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
