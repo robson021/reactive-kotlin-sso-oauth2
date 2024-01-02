530024d5924a521492b5be0c8787d704cb90fae6
Robert Nowak <robertn@amazon.com>
2024-01-02 01:37:29 +0100

    move config to single file

--- src/main/kotlin/com/example/ssodemo/SsoDemoApplication.kt
+++ src/main/kotlin/com/example/ssodemo/SsoDemoApplication.kt
@@ -2,6 +2,7 @@ package com.example.ssodemo
 
 import com.example.ssodemo.db.UserDetails
 import com.example.ssodemo.extensions.userDetails
+import com.example.ssodemo.model.GoogleAuthCredentials
 import io.r2dbc.spi.ConnectionFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -10,9 +11,17 @@ import org.springframework.core.io.ClassPathResource
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
 import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
 import org.springframework.security.oauth2.client.registration.ClientRegistration
+import org.springframework.security.oauth2.client.registration.InMemoryReactiveClientRegistrationRepository
+import org.springframework.security.oauth2.core.AuthorizationGrantType
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod
+import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames
 import org.springframework.security.oauth2.core.user.OAuth2User
+import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.Instant
@@ -21,11 +30,43 @@ import java.util.*
 
 
 @SpringBootApplication
+@EnableWebFluxSecurity
 @EnableR2dbcRepositories
 class SsoDemoApplication
 
 fun main(args: Array<String>) {
     runApplication<SsoDemoApplication>(*args)
+}
+
+@Bean
+fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+    http
+        .authorizeExchange { authorize: ServerHttpSecurity.AuthorizeExchangeSpec ->
+            authorize
+                .pathMatchers("/").permitAll()
+                .anyExchange().authenticated()
+        }
+        .oauth2Login(Customizer.withDefaults())
+    return http.build()
+}
+
+@Bean
+fun clientRegistrationRepository(googleAut: GoogleAuthCredentials): InMemoryReactiveClientRegistrationRepository {
+    val googleClient = ClientRegistration.withRegistrationId("google")
+        .clientId(googleAut.clientId)
+        .clientSecret(googleAut.clientSecret)
+        .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+        .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+        .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
+        .scope("openid", "profile", "email")
+        .authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
+        .tokenUri("https://www.googleapis.com/oauth2/v4/token")
+        .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
+        .userNameAttributeName(IdTokenClaimNames.SUB)
+        .jwkSetUri("https://www.googleapis.com/oauth2/v3/certs")
+        .clientName("Google")
+        .build()
+    return InMemoryReactiveClientRegistrationRepository(googleClient)
 }
 
 @Bean
