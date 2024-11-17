142f402c9a50b67db75f5e5cfb81e010972e8697
robson021 <nowak021@gmail.com>
2024-11-17 15:59:42 +0100

    remove type from logger helper function

--- src/main/kotlin/com/example/ssodemo/extensions/LoggerExtensions.kt
+++ src/main/kotlin/com/example/ssodemo/extensions/LoggerExtensions.kt
@@ -3,7 +3,7 @@ package com.example.ssodemo.extensions
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-inline fun <reified R : Any> R.getLogger(): Lazy<Logger> = lazy {
+inline fun <reified R : Any> R.getLogger() = lazy {
     LoggerFactory.getLogger(this::class.java.name.removeSuffix("\$Companion"))
 }
 
